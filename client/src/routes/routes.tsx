f431357e151fd8e65212867a93fbf9e6f187875e
happycloudy <arion.mikola@gmail.com>
2023-08-27 02:37:53 +0300

    add routes

--- client/src/routes/routes.tsx
+++ client/src/routes/routes.tsx
@@ -1,9 +1,14 @@
 import Game from "../pages/Game.tsx";
 import {createBrowserRouter, RouteObject} from "react-router-dom";
+import Home from "../pages/Home.tsx";
 
 export const routes: RouteObject[] = [
     {
         path: '/',
+        element: <Home/>
+    },
+    {
+        path: '/game',
         element: <Game/>
     }
 ]
