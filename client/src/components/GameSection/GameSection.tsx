e2994c57b06300bb08d69169cd4b3f9351c4f6d9
happycloudy <arion.mikola@gmail.com>
2023-08-31 04:08:14 +0300

    err fix and include build in server statics

--- client/src/components/GameSection/GameSection.tsx
+++ client/src/components/GameSection/GameSection.tsx
@@ -1,6 +1,5 @@
-import {ReactNode, useMemo} from "react";
+import {ReactNode} from "react";
 import styles from './game-section.module.css'
-import * as classNames from "classnames";
 
 type IProps = {
     children: ReactNode
@@ -8,12 +7,8 @@ type IProps = {
 }
 
 const GameSection = ({children, centered}: IProps) => {
-    const sectionClasses = useMemo(() => classNames(styles.section, {
-        [styles.centered]: centered
-    }), [centered])
-
     return (
-        <section className={sectionClasses}>
+        <section className={`${styles.section} ${centered ? styles.centered : ''}`}>
             {children}
         </section>
     );
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { GamesModule } from './games/games.module';
 import { GameModule } from './game/game.module';
 import { ApiModule } from './api/api.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [GamesModule, GameModule, ApiModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'static'),
+    }),
+    GamesModule,
+    GameModule,
+    ApiModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
